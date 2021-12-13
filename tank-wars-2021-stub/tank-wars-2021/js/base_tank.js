8bb33b61922573564da2752eb71ea7e4c646e6d1
GEORGE-SCUTARU <88233902+GEORGE-SCUTARU@users.noreply.github.com>
2021-12-13 12:31:35 +0000

    Commit to main
    session-1

---
+++ tank-wars-2021-stub/tank-wars-2021/js/base_tank.js
@@ -0,0 +1,32 @@
+class BaseTank {
+
+    /** @type {number} */
+    tankspeed = 100
+
+    /** @type {number} */
+    damageMax = 2
+    constructor(scene, x, y, texture, frame){
+
+    }
+    update(){
+
+    }
+    damage(){
+
+    }
+    setBullets(){
+
+    }
+    burn(){
+
+    }
+    isDestroyed(){
+
+    }
+    isImmobilized(){
+
+    }
+    enableCollision(){
+    
+    }
+}
